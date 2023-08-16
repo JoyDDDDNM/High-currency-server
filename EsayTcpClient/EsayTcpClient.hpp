16daea07c83807a4acbe9d367f67b3fbc8b43aee
Ruidong Wu <yainwrd@gmail.com>
2023-08-17 03:19:55 +1000

    encapsulate client into class

---
+++ EsayTcpClient/EsayTcpClient.hpp
@@ -0,0 +1,204 @@
+#ifndef _EasyTcpClient_hpp_
+#define _EasyTcpClient_hpp_
+
+#ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN // macro to avoid including duplicate macro when include <windows.h> and <WinSock2.h>
+	#include <windows.h>  // windows system api
+	#include <WinSock2.h> // windows socket api 
+#else
+	#include <unistd.h> // unix standard system interface
+	#include <arpa/inet.h>
+	#include <string>
+
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include "Message.hpp"
+
+class EasyTcpClient
+{
+public:
+	EasyTcpClient() :_sock{ INVALID_SOCKET } {};
+
+	// initialize socket of client to connect server
+	int initSocket() {
+		// lanuch Win Sock 2.x environment
+#ifdef _WIN32
+		// launch windows socket 2.x environment
+		WORD ver = MAKEWORD(2, 2);
+		WSADATA dat;
+
+		// initiates use of the Winsock DLL by program.
+		WSAStartup(ver, &dat);
+#endif
+		// socket has been created, close it and create an new one
+		if (INVALID_SOCKET != _sock) {
+			std::cout << "socket: " << _sock << " has been created previously, close it and recreate an new socket" << std::endl;
+			closeSock();
+		}
+		
+		// 1. build socket
+		_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (INVALID_SOCKET == _sock) {
+			std::cout << "socket create failed" << std::endl;
+		}
+		else {
+			std::cout << "socket create succeed" << std::endl;
+		}
+
+		return 0;
+	}
+
+	// connect to server with sepcfied ip and port number
+	int connectServer(const char* ip,unsigned short port) {
+
+		if (INVALID_SOCKET == _sock) {
+			std::cout << "socket did not created, create socket automatically for connecting server" << std::endl;
+			initSocket();
+		}
+
+		// 2. connect server
+		// https://stackoverflow.com/questions/21099041/why-do-we-cast-sockaddr-in-to-sockaddr-when-calling-bind
+		sockaddr_in _sin = {};
+		_sin.sin_family = AF_INET;
+		_sin.sin_port = htons(port);
+
+#ifdef _WIN32
+		_sin.sin_addr.S_un.S_addr = inet_addr(ip);
+#else
+		_sin.sin_addr.s_addr = inet_addr(ip);
+#endif
+
+		int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+
+		if (SOCKET_ERROR == ret) {
+			std::cout << "server connect failed" << std::endl;
+		}
+		else {
+			std::cout << "server connect succeed" << std::endl;
+		}
+
+		return ret;
+	}
+
+	// close client socket
+	void closeSock() {
+		if (INVALID_SOCKET != _sock) {
+#ifdef _WIN32
+			closesocket(_sock);
+			WSACleanup();
+#else
+			close(_sock);
+#endif
+			_sock = INVALID_SOCKET;
+		}
+	}
+
+	// listen response from server
+	bool listenServer() {
+		// socket is not created
+		if (!isRun()) {
+			return false;
+		}
+		fd_set fdRead;
+		FD_ZERO(&fdRead);
+		FD_SET(_sock, &fdRead);
+
+		timeval t = { 1, 0 };
+
+		// client is blocked
+		int ret = select(_sock + 1, &fdRead, 0, 0, &t);
+		if (ret < 0) {
+			std::cout << "server is closed " << std::endl;
+			return false;
+		}
+
+		if (FD_ISSET(_sock, &fdRead)) {
+			FD_CLR(_sock, &fdRead);
+
+			if (receiveServerMessage(_sock) == -1) {
+				return false;
+			};
+		}
+
+		return true;
+	}
+
+	// receive message from server, solve message concatenation
+	int receiveServerMessage(SOCKET _cSock) {
+		// buffer for receiving data, this is still a fixed length buffer
+		char szRecv[1024] = {};
+
+		// 5. keeping reading message from clients
+		//we only read header info from the incoming message
+		int nLen = (int)recv(_cSock, (char*)szRecv, sizeof(DataHeader), 0);
+		DataHeader* header = (DataHeader*)szRecv;
+		if (nLen <= 0) {
+			// connection has closed
+			std::cout << "server disconnected" << std::endl;
+			return -1;
+		}
+		recv(_cSock, szRecv + sizeof(DataHeader), header->length - sizeof(DataHeader), 0);
+		
+		processServerMessage(header);
+		return 0;
+	}
+
+	bool isRun() {
+		return _sock != INVALID_SOCKET;
+	}
+
+	// prompt message to client for the received message from server
+	void processServerMessage(DataHeader* header) {
+		switch (header->cmd) {
+			case CMD_LOGIN_RESULT: {
+			
+				LoginRet* loginRet = (LoginRet*)header;
+				std::cout << "Receive message from server: login successfully" << std::endl;
+				std::cout << "Message length: " << loginRet->length << std::endl;
+				break;
+			}
+			case CMD_LOGOUT_RESULT: {
+				LogoutRet* logoutRet = (LogoutRet*)header;
+				std::cout << "Receive message from server: logout successfully" << std::endl;
+				std::cout << "Message length: " << logoutRet->length << std::endl;
+				break;
+			}
+			case CMD_NEW_USER_JOIN: {
+				NewUserJoin* newUser = (NewUserJoin*)header;
+				std::cout << "Receive message from server: new user join into server" << std::endl;
+				std::cout << "user id: " << newUser->cSocket << std::endl;
+				std::cout << "Message length: " << newUser->length << std::endl;
+				break;
+			}
+			default: {
+
+				break;
+			}
+		}
+	}
+
+	int sendMessage(DataHeader* header) {
+
+		if (isRun() && header) {
+			send(_sock, (const char*)header, header->length, 0);
+			
+		}
+
+		return SOCKET_ERROR;
+	}
+
+	virtual ~EasyTcpClient() {
+		closeSock();
+	}
+
+private:
+	SOCKET _sock;
+};
+
+#endif
--- EsayTcpClient/client.cpp
+++ EsayTcpClient/client.cpp
@@ -6,153 +6,29 @@
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
-
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN // macro to avoid including duplicate macro when include <windows.h> and <WinSock2.h>
-    #include <windows.h>  // windows system api
-    #include <WinSock2.h> // windows socket api 
-#else
-    #include <unistd.h> // unix standard system interface
-    #include <arpa/inet.h>
-    #include <string>
-    
-    #define SOCKET int
-    #define INVALID_SOCKET  (SOCKET)(~0)
-    #define SOCKET_ERROR            (-1)
-#endif
-
-#include <iostream>
+#include "EsayTcpClient.hpp"
 #include <thread>
-#include <vector>
 
-enum CMD {
-    CMD_LOGIN,
-    CMD_LOGIN_RESULT,
-    CMD_LOGOUT,
-    CMD_LOGOUT_RESULT,
-    CMD_NEW_USER_JOIN,
-    CMD_ERROR
-};
-
-struct DataHeader {
-    short length;
-    short cmd;
-};
-
-struct Login : public DataHeader {
-    Login() {
-        length = sizeof(Login);
-        cmd = CMD_LOGIN;
-    }
-    char userName[32];
-    char password[32];
-};
-
-struct LoginRet : public DataHeader {
-    LoginRet() {
-        length = sizeof(LoginRet);
-        cmd = CMD_LOGIN_RESULT;
-        result = 0;
-    }
-    int result;
-};
-
-struct Logout : public DataHeader {
-    Logout() {
-        length = sizeof(Logout);
-        cmd = CMD_LOGOUT;
-    }
-    char userName[32];
-};
-
-struct LogoutRet : public DataHeader {
-    LogoutRet() {
-        length = sizeof(LogoutRet);
-        cmd = CMD_LOGOUT_RESULT;
-        result = 0;
-    }
-    int result;
-};
-
-struct NewUserJoin : public DataHeader {
-    NewUserJoin() {
-        length = sizeof(NewUserJoin);
-        cmd = CMD_NEW_USER_JOIN;
-        cSocket = 0;
-    }
-    int cSocket;
-};
-
-
-int receiveServerMessage(SOCKET _cSock) {
-
-    // buffer for receiving data, this is still a fixed length buffer
-    char szRecv[1024] = {};
-
-    // 5. keeping reading message from clients
-    //we only read header info from the incoming message
-    int nLen = (int)recv(_cSock, (char*)szRecv, sizeof(DataHeader), 0);
-    DataHeader* header = (DataHeader*)szRecv;
-    if (nLen <= 0) {
-        // connection has closed
-        std::cout << "server disconnected" << std::endl;
-        return -1;
-    }
-
-    switch (header->cmd) {
-        case CMD_LOGIN_RESULT: {
-            recv(_cSock, szRecv + sizeof(DataHeader), header -> length - sizeof(DataHeader), 0);
-            LoginRet* loginRet = (LoginRet*)szRecv;
-            std::cout << "Receive message from server: login successfully" << std::endl;
-            std::cout << "Message length: " << loginRet->length << std::endl;
-            break;
-        }
-        case CMD_LOGOUT_RESULT: {
-            recv(_cSock, szRecv + sizeof(DataHeader), header->length - sizeof(DataHeader), 0);
-            LogoutRet* logoutRet = (LogoutRet*)szRecv;
-            std::cout << "Receive message from server: logout successfully" << std::endl;
-            std::cout << "Message length: " << logoutRet->length << std::endl;
-            break;
-        }
-        case CMD_NEW_USER_JOIN: {
-            recv(_cSock, szRecv + sizeof(DataHeader), header->length - sizeof(DataHeader), 0);
-            NewUserJoin* newUser = (NewUserJoin*)szRecv;
-            std::cout << "Receive message from server: new user join into server" << std::endl;
-            std::cout << "user id: " << newUser -> cSocket << std::endl;
-            std::cout << "Message length: " << newUser->length << std::endl;
-            break;
-        }
-        default: {
-       
-            break;
-        }
-    }
-
-    return 0;
-}
-
-bool isNotExit = true;
-
-void cmdThread(SOCKET _sock) {
+void cmdThread(EasyTcpClient* client) {
     while (true) {
         char cmdBuf[256] = {};
         std::cin >> cmdBuf;
         if (strcmp(cmdBuf, "exit") == 0) {
             std::cout << "sub thread finished" << std::endl;
             // tell main thread that the sub thread is finished
-            isNotExit = false;
+            client->closeSock();
             break;
         }
         else if (strcmp(cmdBuf, "login") == 0) {
             Login login;
             strcpy(login.userName, "account");
             strcpy(login.password, "password");
-            send(_sock,(const char*)&login, sizeof(Login),0);
+            client->sendMessage(&login);
         }
         else if (strcmp(cmdBuf, "logout") == 0) {
             Logout logout;
             strcpy(logout.userName, "account");
-            send(_sock, (const char*)&logout, sizeof(Logout), 0);
+            client->sendMessage(&logout);
         }
         else {
             std::cout << "not valid command" << std::endl;
@@ -162,88 +38,33 @@ void cmdThread(SOCKET _sock) {
 
 int main()
 {
-#ifdef _WIN32
-    // launch windows socket 2.x environment
-    WORD ver = MAKEWORD(2, 2);
-    WSADATA dat;
+    EasyTcpClient client;
 
-    // initiates use of the Winsock DLL by program.
-    WSAStartup(ver, &dat);
-#endif
+    client.initSocket();
 
-    // 1. build socket
-    SOCKET _sock = socket(AF_INET,SOCK_STREAM,0);
-    if (INVALID_SOCKET == _sock) {
-        std::cout << "socket create failed" << std::endl;
-    }
-    else {
-        std::cout << "socket create succeed" << std::endl;
-    }
+    client.connectServer("127.0.0.1",4567);
 
-    // 2. connect server
-    // https://stackoverflow.com/questions/21099041/why-do-we-cast-sockaddr-in-to-sockaddr-when-calling-bind
-    sockaddr_in _sin = {};
-    _sin.sin_family = AF_INET;
-    _sin.sin_port = htons(4567); 
+    // we can add more clients to connnect different servers or ports
+    // EasyTcpClient client2
+    // client2.initSocket
+    // client2.connectServer("127.0.0.1", 4567);
+    // std::thread  clientCmdThread2(cmdThread, &client2);
 
-#ifdef _WIN32
-    _sin.sin_addr.S_un.S_addr = inet_addr("192.168.0.100");
-#else
-    _sin.sin_addr.s_addr = inet_addr("192.168.0.100");
-#endif
-
-    int ret = connect(_sock,(sockaddr*)&_sin, sizeof(sockaddr_in));
-
-    if (SOCKET_ERROR == ret) {
-        std::cout << "server connect failed" << std::endl;
-    }
-    else {
-        std::cout << "server connect succeed" << std::endl;
-    }
-    
-    
     // create an thread for reading client input
-    std::thread  clientCmdThread(cmdThread,_sock);
+    std::thread  clientCmdThread(cmdThread,  &client);
     
     // Separates the thread of execution from the thread object, 
     // allowing execution to continue independently
     clientCmdThread.detach();
 
     // 3.process user input request
-    while (isNotExit) {
-
-        fd_set fdRead;
-        FD_ZERO(&fdRead);
-        FD_SET(_sock, &fdRead);
-
-        timeval t = { 1, 0 };
-
-        // client is blocked
-        int ret = select(_sock + 1, &fdRead, 0, 0, &t);
-        if (ret < 0) {
-            std::cout << "server is closed " << std::endl;
-            break;
-        }
-
-        if (FD_ISSET(_sock, &fdRead)) {
-            FD_CLR(_sock, &fdRead);
-
-            if (receiveServerMessage(_sock) == -1) {
-                break;
-            };
-        }
-
+    while (client.isRun()) {
+        client.listenServer();
+       
         //std::cout << "Client is idle and able to deal with other tasks" << std::endl;
     }
 
-    // 5. close socket 
-
-#ifdef _WIN32
-    closesocket(_sock);
-    WSACleanup();
-#else
-    close(_sock);
-#endif
+    client.closeSock();
 
     std::cout << "Client exit" << std::endl;
     std::cout << "Press Enter to exit" << std::endl;
